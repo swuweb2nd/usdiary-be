048f35dc45d81d428e9f36929fe9f927bc563c81
jjaeroong <jy8294@naver.com>
2024-10-28 12:39:24 +0000

    ♿️ 서버/클라이언트 배포

--- swagger/swagger.js
+++ swagger/swagger.js
@@ -7,11 +7,11 @@ const options = {
   },
   servers: [
     {
-      "url": "http://localhost:3001",
+      "url": "http://localhost:3000",
       "description": "Local development server"
     },
     {
-      "url": "https://api.example.com",
+      "url": "https://api.usdiary.site",
       "description": "Production server"
     }
   ],
